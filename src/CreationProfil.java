ff200e9978e41a0dca33cfed5fb63dfbd4fc4f21
Tiphaine Meriot <meriottiphaine@gmail.com>
2023-03-11 11:50:10 +0100

    Premiere version creation profil

--- src/CreationProfil.java
+++ src/CreationProfil.java
@@ -1,14 +1,17 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class CreationProfil extends Application {
 
     @Override
-    public void start(Stage p) throws Exception {
-        p.setWidth(500);
-        p.setHeight(500);
-        p.setTitle("Créer un profil");
-        p.show();
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(CreationProfil.class.getResource("creationp.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setTitle("Créer un nouveau profil");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
