8f913fddaf284ec590d03de8c68e6899ecda6aa5
Noe <lauras.noe@gmail.com>
2023-03-14 20:53:02 +0100

    Ajout de la vue

--- src/Vue.java
+++ src/Vue.java
@@ -1,2 +1,21 @@
-public class Vue {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Vue extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(CreationProfil.class.getResource("Vue.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200 , 800);
+        stage.setResizable(false);
+        stage.setTitle("Vue");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
