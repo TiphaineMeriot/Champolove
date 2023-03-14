5fbe5894f60dfa142de50f4d4ab57540c2f265f5
Noe <lauras.noe@gmail.com>
2023-03-14 21:40:11 +0100

    Ajout d'un Frame Authentification

--- src/Authentification.java
+++ src/Authentification.java
@@ -1,2 +1,15 @@
-public class Authentification {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Authentification extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Authentification.class.getResource("authentification.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 693 , 488);
+        stage.setResizable(false);
+        stage.setTitle("Authentification");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
