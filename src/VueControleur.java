f8b360b35211c7103977d82bb3a6804404c81d45
Noe <lauras.noe@gmail.com>
2023-03-27 08:35:34 +0200

    Ajout de la fonctionnalité clic sur profil --> affichage des infos du profil associé.
    Version ultra simpliste je garde les autres pour l'instant car elles sont pas encore fonctionnelles a 100%

--- src/VueControleur.java
+++ src/VueControleur.java
@@ -14,7 +14,7 @@ public class VueControleur {
 
     Button boutonCreerProfil;
     String profilRecherche;
-    String profilClick;
+    Profil profilClick;
 
     Generateur_profil generateurProfil;
 
@@ -54,10 +54,46 @@ public class VueControleur {
             imageView.setFitHeight(70);
             imageView.setFitWidth(70);
             gridPane.add(imageView, 0, i);
+            //si la souris clique sur l'image,on stocke le profil correspondant dans la variable profilClick
+            imageView.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
+                @Override
+                public void handle(javafx.scene.input.MouseEvent event) {
+                    //TODO : Afficher le profil correspondant dans la zone de droite
+                    try {
+                        // on instancie un nouveau profil avec les informations du profil cliqué
+                        profilClick = new Profil(profil.nom, profil.prenom, profil.date_de_naissance, profil.genre, profil.statut, profil.ville, profil.recherche);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    //on ajoute dans le pane de droite les informations de profilClick avec des labels
+                    Label labelNom = new Label(profilClick.nom);
+                    Label labelPrenom = new Label(profilClick.prenom);
+                    Label labelGenre = new Label(profilClick.genre);
+                    Label labelDateDeNaissance = new Label(profilClick.date_de_naissance);
+                    Label labelStatut = new Label(profilClick.statut);
+                    Label labelVille = new Label(profilClick.ville);
+                    Label labelRecherche = new Label(profilClick.recherche);
+
+                    // on clear le pane de droite
+                    GridPane gridPaneDroite = (GridPane) scene.lookup("#GridPaneDroite");
+                    gridPaneDroite.getChildren().clear();
+                    //on ajoute les labels dans le pane de droite
+
+                    gridPaneDroite.add(labelNom, 0, 0);
+                    gridPaneDroite.add(labelPrenom, 0, 1);
+                    gridPaneDroite.add(labelGenre, 0, 2);
+                    gridPaneDroite.add(labelDateDeNaissance, 0, 3);
+                    gridPaneDroite.add(labelStatut, 0, 4);
+                    gridPaneDroite.add(labelVille, 0, 5);
+                    gridPaneDroite.add(labelRecherche, 0, 6);
+
+                }
+            });
             i++;
         }
 
         //TODO : Ajouter un eventlistener sur les images pour afficher le profil correspondant dans la zone de droite
+
 
 
     }
