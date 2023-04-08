d7448e1813ae25dcb53d18a9710ee03afb5f3f2f
Tiphaine Meriot <meriottiphaine@gmail.com>
2023-04-08 18:36:04 +0200

    modif

--- src/CreationProfil.java
+++ src/CreationProfil.java
@@ -1,7 +1,11 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 
@@ -13,11 +17,11 @@ public class CreationProfil extends Application {
         Parent root = loader.load();
         CreationProfilControleur controleur = loader.getController();
         controleur.initialisationComboBox();
-        //controleur.init();
         stage.setScene(new Scene(root));
         stage.show();
         stage.setResizable(false);
         stage.setTitle("Créer un nouveau profil");
+
 
     }
 
